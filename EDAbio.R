b0cdc2a651967d63fc9be9c63a0720cc8a502f33
Luis Chaves <lc5415@ic.ac.uk>
2020-01-31 12:44:04 +0000

    Improved preprocessing of biomarkers and covariates

--- EDAbio.R
+++ EDAbio.R
@@ -17,24 +17,53 @@ if (!require(ggbiplot)) install_github("vqv/ggbiplot")
 library(ggbiplot)
 if (!require(GGally)) install_github("GGally")
 library(GGally)
+if (!require(tidyverse)) install("tidyverse")
+library(tidyverse)
+if (!require(naniar)) install("naniar")
+library(naniar)
 
 
+## LOAD DATASETS ####
 # Load datasets, when doing actual analysis, replace these by the non-toy datasets
-b = readRDS("data/Biomarkers_toy.rds")
-c = readRDS("data/Covars_toy.rds")
-b.example = readRDS("data/Biomarkers_toy_example.rds")
+c.original = readRDS("data/Covars_toy.rds")
+b.original= readRDS("data/Biomarkers_toy_example.rds")
 bio.dict = readxl::read_xlsx("Biomarker_annotation.xlsx")
-cov.dict = readxl::read_xlsx("Covariate_dictionary.xlsx")
-b$id = rownames(b) 
 
-# Some of b columns are character eventhough they are numeric measures,
-# make those into numeric and put back into a df
-b = as.data.frame(apply(b,2,as.numeric))
+cov.dict = readxl::read_xlsx("Covariate_dictionary.xlsx")
+
+## Initial pre-processing ####
+#make nicely looking names (programmingly functional)
+colnames(bio.dict) = make.names(colnames(bio.dict), unique=TRUE)
+
+#get column numbers of columns with name containing pattern *(.)1(.)*
+# use (.) to match the dot as opposed to using . as a wildcard
+b = b.original[,!grepl("*(.)1(.)0", colnames(b))]
+
+# Match code with biomarker name to change column names of b
+# get element 2 to 6 of all string in vector colnames(b)
+# the match() function, match the substring from colnames to
+# the UK.biobank.field in the biomarkers dictionary, 
+# effectively ordering the colnames of b
+# Alternative: order UK.bionbank.field entries and match them
+#---- bio.dict = bio.dict %>% arrange(UK.Biobank.Field)
+
+colnames(b) = bio.dict$Biomarker.name[
+  match(substring(colnames(b),2,6),bio.dict$UK.Biobank.Field)] 
+
+# check number of missing values per column
+colSums(is.na(b))
 
 # safety-check for all vars being numeric
 stopifnot(all(apply(b, 2, is.numeric)))
 
+## preprocessing c
+# replace empty strings by NA
+values_to_replace_w_na = c("")
+c = c.original %>% replace_with_na_all(condition = ~.x %in% values_to_replace_w_na)
+#remove anything to do wih cancer or external deaths
+c = c[,!grepl("cancer|external", colnames(c))]
 
+## Second pre-processing ####
 #remove missing values, this can later be replace by imputation to compare results,
 # MAR is probably the approach to take as we are dealing with biochemical measurements
 # not humans
