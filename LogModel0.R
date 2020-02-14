cdf249598a81ef2365eb2378314491f0c602a039
Luis Chaves <lc5415@ic.ac.uk>
2020-02-14 16:25:47 +0000

    deleted a couple files

--- LogModel0.R
+++
@@ -1,8 +0,0 @@
-
-suppressPackageStartupMessages(library(tidyverse))
-
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-covars = readRDS("data/Covars_toy.rds")
-
-# Logistic model here
-
