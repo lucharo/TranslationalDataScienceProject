ba99778046a62d8f8c184795da5815e76a56ae6a
catrionamackay <catriona1296@gmail.com>
2020-02-19 13:57:35 +0000

    Finished processing snp data

--- Polygenic risk score.R
+++ Polygenic risk score.R
@@ -1,5 +1,11 @@
+# Aim of this script is to calculate a polygenic risk score for each person
 
+##################################################################
+##                 Prepare libraries and data                   ##
+##################################################################
 
 rm(list=ls())
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 snp_info <- readRDS("data/preprocessed/snpInfo.rds")
+snp <- readRDS("data/preprocessed/snpProcessed.rds")
