990534546d23228bb0f4da1b1860144757f0df14
Luis Chaves <lc5415@ic.ac.uk>
2020-02-23 22:22:38 +0000

    changed the code of PRS a bit as it was not working on my laptop, also added some comments regarding naming of variables

--- Polygenic risk score.R
+++ Polygenic risk score.R
@@ -7,6 +7,8 @@
 rm(list=ls())
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+library(ggplot2)
+
 snp.info <- readRDS("data/preprocessed/snpInfo.rds")
 snp <- readRDS("data/preprocessed/snpProcessed.rds")
 
@@ -21,14 +23,17 @@ stopifnot(all(colnames(snp) == snp.info$markername))
 #Extracting the beta values (which are in the correct order based on above check)
 betas <- snp.info$beta
 
-#Element-wise multiplication of each row of snp by the betas (margin=2 specifies it is rows of the matrix; each row is a person) - this multiples each no. of snp copies by the beta coefficient for that snp 
+#Element-wise multiplication of each row of snp by the betas
+# (margin=2 specifies it is rows of the matrix; each row is a person)
+# this multiples each no. of snp copies by the beta coefficient for 
+# that snp 
 PRS_matrix = sweep(snp, MARGIN=2, betas, `*`)
 
 
 #Now calculate the weighted sum for each person (have included na.rm=TRUE to make it work for now, can remove when we've done imputation)
-PRS_sums = rowSums(PRS_int, na.rm=TRUE)
+PRS_sums = rowSums(PRS_matrix, na.rm=TRUE)
 
-PRS = cbind(rownames(snp), PRSsums)
+PRS = cbind(rownames(snp), PRS_sums)
 saveRDS(PRS, "data/preprocessed/PolygenicRiskScore.rds")
 
 
@@ -37,10 +42,14 @@ saveRDS(PRS, "data/preprocessed/PolygenicRiskScore.rds")
 #################################################################
 
 cov <- readRDS("data/preprocessed/covProcessed.rds")
-cov.prs <- cbind(cov, PRS)
+
+## CODE WARNING: careful with the name of stuff, check the second 
+# to last column of cov.prs (it's called V1)
+cov.prs <- cbind(cov, PRS['PRS_Sums'])
 
 #Boxplot of PRS by CVD status
-ggplot(cov.prs, aes(x=CVD_status, y=PRS)) + geom_boxplot()
+ggplot(cov.prs, aes(x=CVD_status, y=PRS_sums))+
+  geom_boxplot()
 
 #t-test - no sig difference in mean PRS between groups...
 t.test(PRS ~ CVD_status, data=cov.prs)
