b27fa34ecf49a4666dd84d991a88b270552f026c
Luis Chaves <lc5415@login-7.rcs.ic.ac.uk>
2020-02-28 01:00:36 +0000

    moved all scripts to scripts folder:

---
+++ Scripts/XGBoost_cov.R
@@ -0,0 +1,249 @@
+
+# Installing and Loading Packages -----------------------------------------
+if (!require(xgboost)) install_github("xgboost")
+library(xgboost)
+if (!require(readr)) install_github("readr")
+library(readr)
+if (!require(stringr)) install.packagaes("stringr")
+library(stringr)
+if (!require(caret)) install.packages("caret")
+library(caret)
+if (!require(car)) install.packages("car")
+library(car)
+
+library(tidyverse)
+
+
+# Setting up our environment ----------------------------------------------
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+#Load Data
+cov = readRDS("data/preprocessed/covProcessed.rds")
+bio = readRDS("data/preprocessed/bioImputed.rds")
+
+# Preparing our Data and selecting features -------------------------------
+
+# One stumbling block when getting started with the xgboost package in R is 
+# that you can't just pass it a dataframe. The core xgboost function requires data to be a matrix.
+
+#To prepare our data, we have a number of steps we need to complete:
+#remove information about the target variable from the training data
+#reduce the amount of redundant information 
+#convert categorical information to a numeric format
+#Split dataset into training and testing subsets
+#Convert the cleaned dataframe to a Dmatrix
+
+
+# Remove information about the target variable from the training d --------
+
+#First let's remove the columns that have information on our target variable
+
+#Let's create a new vector with labels - convert the CVD_status factor to an 
+# integer class starting at 0, as the first class should be 0; picky requirement
+Outcome = as.integer(cov$CVD_status)-1
+
+cov <- cov %>% 
+  select(-c("vit_status","dc_cvd_st", "CVD_status","age_cl", "stop","stop_cvd",
+            "age_CVD", "cvd_final_icd10", "primary_cause_death_ICD10",
+            "cvd_death", "cvd_incident", "cvd_prevalent"))
+
+# Reduce the amount of redundant information ------------------------------
+
+#Finally, I want to remove all the non-numeric variables,
+# since a matrix can only hold numeric variables - if you try to create a 
+# matrix from a dataframe with non-numeric variables in it, it will 
+# conver them all into NA's and give you some warning messages.
+
+# Luis: we want to keep ordered categorical variables as integers but OneHotEncoding
+# on non-ordered categorical variables (e.g. smok_ever, physical activity) only those
+# 2 surprisingly
+
+# ONCE transformed make into matrix --> 
+
+# Convert dataframe into matrix -------------------------------------------
+
+cov <- data.matrix(cov)
+
+
+# Split dataset into Training and Testing Sets ----------------------------
+
+#Get the numb 70/30 training test split
+tr.index <- sample(1:nrow(cov_, size = 0.7*nrow(cov)))
+
+#Training Data
+cov.train <- cov[tr.index, ]
+y.train <- Outcome[tr.index]
+
+#Testing Data
+cov.test <- cov[-tr.index, ]
+test_labels <- Outcome[-tr.index]
+
+
+# Convert the cleaned dataframe to a DMATRIX ------------------------------
+
+# put our testing & training data into two seperates Dmatrixs objects
+dtrain <- xgb.DMatrix(data = train_data, label= train_labels)
+dtest <- xgb.DMatrix(data = test_data, label= test_labels)
+
+
+# Training our model ------------------------------------------------------
+
+#In order to train our model, we need to give it some information to start with. It needs to know:
+
+#What training data to use. In this case, we've already put our data in a dmatrix and can just pass it that.
+#The number of training rounds. This just means the number of times we're going to improve our naive model by adding additional models. In the figure above going around the circle once = one boosting round.
+#What the objective function is. The objective function you pick will depend on the task you have. Because we're going to try and predict something that's binary (either humans get sick or they don't), we're going to use “binary:logistic”, which is logistic regression for binary (two-class) classification. By default, xgboost will do linear regression.
+
+# train a model using our training data
+model <- xgboost(data = dtrain, # the data   
+                 nround = 2, # max number of boosting iterations
+                 objective = "binary:logistic")
+
+# generate predictions for our held-out testing data
+pred <- predict(model, dtest)
+
+# get & print the classification error
+err <- mean(as.numeric(pred > 0.5) != test_labels)
+print(paste("test-error=", err))
+
+
+# Tuning our model --------------------------------------------------------
+
+#By default, the max depth of trees in xgboost is 6 - let's set it to 3 instead
+
+# train an xgboost model
+model_tuned <- xgboost(data = dtrain, # the data           
+                       max.depth = 3, # the maximum depth of each decision tree
+                       nround = 2, # max number of boosting iterations
+                       objective = "binary:logistic")
+
+# generate predictions for our held-out testing data
+pred <- predict(model_tuned, dtest)
+
+# get & print the classification error
+err <- mean(as.numeric(pred > 0.5) != test_labels)
+print(paste("test-error=", err))
+
+#Hence we have not over-fitted
+
+
+# Examining our model -----------------------------------------------------
+
+# plot them features! what's contributing most to our model?
+xgb.plot.multi.trees(feature_names = names(covaraites_matrix), 
+                     model = model, fill = TRUE)
+#Doesn't seem to work for some reason???
+
+model_tree <- xgb.dump(model_tuned, with_stats = T)
+model_tree[1:10]
+
+#Get information about how important each feature is
+names <- dimnames(dtrain)[[2]]
+importance_matrix <- xgb.importance(names, model = model_tuned)
+xgb.plot.importance(importance_matrix[1:10,])
+
+
+# Alternative Method ------------------------------------------------------
+
+xgb_params <- list("objective" = "multi:softprob",
+                   "eval_metric" = "mlogloss",
+                   "num_class" = nc)
+
+watchlist <- list(train = dtrain, test = dtest)
+
+#Extreme Gradeint Boosting Model
+bst_model <- xgb.train(params = xgb_params,
+                       data = dtrain, 
+                       nrounds = 100, #Maximum number of iterations
+                       watchlist = watchlist )
+#train-mlogloss = This prints out what the error was on the training data 
+#test-mlogloss = This prints out what the error was on the testing data
+#In the first iteration there is a higher error in the testing data than the training data
+
+#Training and test error plot
+bst_model 
+#Makes use of the evaluation_log results to make it into a plot
+e <- data.frame(bst_model$evaluation_log)
+plot(e$iter, e$train_mlogloss, col = "blue")
+#Fairly high error rate at the beginning but as the iterations increase the error rate significantly does too
+#Now let's add the test error to this plot 
+lines(e$iter, e$test_mlogloss, col = "red")
+
+#Possible over-fitting - need to adjust the model perhaps?
+
+#To see the minimum value of the test-error
+min(e$test_mlogloss)
+#To know which iteration this minimum value belonged to
+e[e$test_mlogloss == 0.179392, ] #Hence the 9th iteration was the lowest
+
+#Now to optimise our model:
+bst_model_1 <- xgb.train(params = xgb_params,
+                       data = dtrain, 
+                       nrounds = 100, #Maximum number of iterations
+                       watchlist = watchlist,
+                       eta = 0.2) #Default value for eta is 0.3 and can range from 0 to 1 - this is a learning rate; so if this value is low it means it is more robust to overfitting - hence can avoid overfitting by keeping this value low)
+bst_model_1
+e_1 <- data.frame(bst_model_1$evaluation_log)
+plot(e_1$iter, e_1$train_mlogloss, col = "blue")
+lines(e_1$iter, e_1$test_mlogloss, col = "red")
+min(e_1$test_mlogloss) 
+e_1[e_1$test_mlogloss == 0.18105, ] #Hence the 14th iteration was the lowest, though the lowest value has increased slightly
+
+#In this case eta = 0.2 may not have been wise - stick with bst_model for now
+
+#Feature Importance 
+imp <-  xgb.importance(colnames(dtrain), model = bst_model)
+print(imp)
+#Gain = improvement in accuracy by a feature to the branches it is on; most important column
+xgb.plot.importance(imp)
+
+
+#Prediction and confusin matrix - test data
+#We will use the test data to create these because it is the most important data
+p <- predict(bst_model, newdata = dtest)
+head(p)
+#The first patients probability is fairly high and therefore is most likely to have CVD? You add them as pairs and will get a probability of 1 - needs to be converted to use it in the confusion matrix
+pred <- matrix(p, nrow = nc, ncol = length(p)/nc) %>% 
+  t() %>% #This will create a transpose for the matrix
+  data.frame() %>% 
+  mutate(label = test_labels, max_prob = max.col(., "last")-1)
+head(pred)
+#"Max_prob" is what the model is predicting and "label" is what it is actually labelled as in the dataset
+table(Prediction = pred$max_prob, Actual = pred$label)
+#Seems like you haven't got the entire dataset as only 600 observations are shown, as opposed to 2000 observations
+
+#More XGBoost Paramaters
+
+#We can add more things onto the bst_model
+bst_model_2 <- xgb.train(params = xgb_params,
+                         data = dtrain, 
+                         nrounds = 100, #Maximum number of iterations
+                         watchlist = watchlist,
+                         eta = 0.2, 
+                         max.depth = 3,#Maximum tree depth and the default value is 6, can be increased or decreased to see if it improves the model; theoretically it can take values from 1 to infinity)
+                         gamma = 0, #Default setting is actually 0, again can range from 1 to infinity; larger values > more conservative algorithm - you can increase gamma to reduce overfitting and to reduce the gap between the blue line and red line in the graph
+                         subsample = 0.5, #Default value is 1, which is 100% and can take values from 0 to 1; lower values -> to prevent overfitting - if it is 0.5 for example the algorithm will randomly collect half of the data in stanses to grow the trees - to prevent overfitting 
+                         colsample_bytree = 1, #Default value is 1
+                         missing = NA, #It guides missing values to left or right and then choose direction with higher gain with regard to the objective - useful when you have a lot of missing data
+                         seed = 333) #Repeat results
+
+#Now do the same as above but with the bst_model_2
+bst_model_2
+e_2 <- data.frame(bst_model_2$evaluation_log)
+plot(e_2$iter, e_2$train_mlogloss, col = "blue")
+lines(e_2$iter, e_2$test_mlogloss, col = "red")
+min(e_2$test_mlogloss)
+e[e_2$test_mlogloss == 0.173852, ] #19th iteration
+imp_2 <-  xgb.importance(colnames(dtrain), model = bst_model_2)
+print(imp_2)
+xgb.plot.importance(imp_2)
+p_2 <- predict(bst_model_2, newdata = dtest)
+head(p_2)
+
+pred_2 <- matrix(p_2, nrow = nc, ncol = length(p_2)/nc) %>% 
+  t() %>% #This will create a transpose for the matrix
+  data.frame() %>% 
+  mutate(label = test_labels, max_prob = max.col(., "last")-1)
+head(pred_2)
+
+table(Prediction = pred_2$max_prob, Actual = pred_2$label)
---
+++ Scripts/preprocessing.R
@@ -0,0 +1,350 @@
+# This file will only include preprocessing of the original datasets and their respective storage
+
+####################################################################
+##  Input this file: Original covariates data set (37 columns),   ##
+##   Original biomarkers(60 columns), Biomarker annotation file,  ##
+##         Covariate dictionary, SNP dataset, SNP info file       ##
+####################################################################
+
+#################################################################################
+##       Output from this file: Processed covariates, only with entries        ##
+##          relevant to CVD[covProcessed.rds], Biomarkers dataset with         ##
+##                   well labelled columns and only one entry                  ##
+##                  over time (not imputed)[bioProcessed.rds],                 ##
+##                     same biomarkers dataset but imputed                     ##
+##          through MICE imputation[bioImputed.rds], biomarker dataset         ##
+##                   with only complete cases [bioMCAR.rds],                   ##
+##   biomarker preprocessed with all columns (30 columns)[bioUnfiltered.rds]   ##
+#################################################################################
+
+###########################################################################
+###########################################################################
+###                                                                     ###
+###                         PACKAGE DECLARATION                         ###
+###                                                                     ###
+###########################################################################
+###########################################################################
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+if (!require(devtools)) install.packages('devtools')
+library(devtools)
+if (!require(remotes)) install.packages('remotes')
+library(remotes)
+if (!require(ggbiplot)) install_github("vqv/ggbiplot")
+library(ggbiplot)
+if (!require(GGally)) install_github("GGally")
+library(GGally)
+if (!require(tidyverse)) install.packagaes("tidyverse")
+library(tidyverse)
+if (!require(naniar)) install.packages("naniar")
+library(naniar)
+if (!require(factoextra)) install.packages("factoextra")
+library(factoextra)
+if (!require(ggfortify)) install.packages("ggfortify")
+library(ggfortify)
+if (!require(stats)) install.packages("stats")
+library(stats)
+if (!require(mice)) install.packages('mice')
+library(mice)
+if (!require(DMwR)) install.packages('DMwR')
+library(DMwR)
+if (!require(impute)) BiocManager::install("impute")
+library(impute)
+
+
+
+library(parallel)
+cores = detectCores()
+
+############################################################################
+############################################################################
+###                                                                      ###
+###                             DATA LOADING                             ###
+###                                                                      ###
+############################################################################
+############################################################################
+
+#################################################################
+##                      Original datasets                      ##
+#################################################################
+cov.original = readRDS("data/Covars_toy.rds")
+bio.original= readRDS("data/Biomarkers_toy.rds")
+bio.dict = readxl::read_xlsx("Biomarker_annotation.xlsx")
+cov.dict = readxl::read_xlsx("Covariate_dictionary.xlsx")
+snp.original = readRDS('data/Genes_toy.rds')
+snp_info.original = readxl::read_xlsx("SNP_info.xlsx")
+
+##################################################################
+##                        Cluster add-in                        ##
+##################################################################
+cluster = 0
+
+if (cluster == 1){
+  cov.original = readRDS("data/Covariates.rds")
+  bio.original= readRDS("data/Biomarkers_full.rds")
+  bio.dict = readxl::read_xlsx("Biomarker_annotation.xlsx")
+  cov.dict = readxl::read_xlsx("Covariate_dictionary.xlsx")
+  snp.original = readRDS('data/genetic_data_cvd_snps.rds')
+  snp_info.original = readxl::read_xlsx("SNP_info.xlsx")
+  
+  rownames(bio.original) = bio.original$`mydata$eid`
+  bio.original = bio.original[,-1]
+}
+
+##################################################################
+
+
+##################################################################
+##              Changing biomarkers codes by names              ##
+##################################################################
+#make nicely looking names (programmingly functional)
+colnames(bio.dict) = make.names(colnames(bio.dict), unique=TRUE)
+
+#get column numbers of columns with name containing pattern *(.)1(.)*
+# use (.) to match the dot as opposed to using . as a wildcard
+bio = bio.original[,!grepl("*(.)1(.)0", colnames(bio.original))]
+
+# Match code with biomarker name to change column names of b
+# get element 2 to 6 of all string in vector colnames(b)
+# the match() function, match the substring from colnames to
+# the UK.biobank.field in the biomarkers dictionary, 
+# effectively ordering the colnames of b
+# Alternative: order UK.bionbank.field entries and match them
+#---- bio.dict = bio.dict %>% arrange(UK.Biobank.Field)
+
+colnames(bio) = bio.dict$Biomarker.name[
+  match(substring(colnames(bio),2,6),bio.dict$UK.Biobank.Field)] 
+
+colnames(bio) = make.names(colnames(bio), unique=TRUE)
+colnames(bio) = sub("\\.\\.",".", colnames(bio))
+
+# safety-check for all vars being numeric
+stopifnot(all(apply(bio, 2, is.numeric)))
+
+
+##################################################################
+##               Processing of covariates dataset               ##
+##################################################################
+## preprocessing c
+# replace empty strings by NA
+values_to_replace_w_na = c("")
+cov = cov.original %>% replace_with_na_all(condition = ~.x %in%
+                                             values_to_replace_w_na)
+#remove anything to do wih cancer or external deaths
+cov = cov[,!grepl("cancer|external", colnames(cov))]
+#remove codes except for icd10
+cov = cov[,!(colnames(cov) %in% c("cvd_final_icd9",
+                                  "cvd_final_nc_illness_code",
+                                  "cvd_final_opcs4",
+                                  "cvd_final_ukb_oper_code",
+                                  "other_cause_death"))]
+
+# change numerical binary outcome variables to categorical
+str(cov)
+cov$CVD_status = as.factor(cov$CVD_status)
+cov$vit_status = as.factor(cov$vit_status)
+cov$dc_cvd_st = as.factor(cov$dc_cvd_st)
+cov$cvd_death = as.factor(cov$cvd_death)
+
+saveRDS(cov, file = "data/preprocessed/covProcessed.rds")
+
+
+##################################################################
+##                   Processing of biomarkers                   ##
+##################################################################
+
+saveRDS(bio, file = "data/preprocessed/bioUnfiltered.rds")
+# drop columns with more than 50% missing values i.e. Rheumatoid factor
+# and oestradiol
+bio = bio[,!colMeans(is.na(bio))>0.5]
+# drop rows with more than 50% biomarkers missing
+bio = bio[!rowMeans(is.na(bio))>0.5,]
+saveRDS(bio, file = "data/preprocessed/bioProcessed.rds")
+
+bioMCAR = bio[complete.cases(bio),]
+saveRDS(bioMCAR, file = "data/preprocessed/bioMCAR.rds")
+
+# impute biomarkers based on biomarkers only
+# t0 = Sys.time()
+# imp.model = mice(bio, m=5, maxit = 10,
+#                  seed = 500, printFlag = F)
+# print(Sys.time() - t0) # takes about 1 minute
+# 
+# # Impute with parallelisation
+# t0 = Sys.time()
+# imp.model = parlmice(bio,  m =5, seed = NA, printFlag = F,
+#                      cl.type = "FORK")
+# print(Sys.time() - t0) # takes about 1 minute
+
+# here we assign the imputed data to bio.imp
+#bio.imp = complete(imp.model,2)
+
+# Impute with KNN -- using caret
+t0 = Sys.time()
+
+kNNImputeOptimization = function(data.in, seed = 1234){
+  set.seed(seed) # this makes the sampling of different indices random
+  # take complete data
+  data.complete = data.in[complete.cases(data.in),]
+  # within the range of rows from data.in grab as many rows as there are
+  # rows in the complete data set
+  rand.index = sample(x = 1:nrow(data.in), size = nrow(data.complete))
+  # get the NA pattern in that random subset of the data.in
+  NApattern = which(is.na(data.in[rand.index,]), arr.ind = T)
+  rows = NApattern[,1]
+  columns = NApattern[,2]
+  # data to impute will be based in complete cases
+  data.to.impute = data.complete
+  # induce missingness in the data to impute with the real structure
+  # of the original data -- feeding these indices does not seem to work
+  # well
+  # -- data.to.impute[rows, columns] = NA
+  
+  ## fill in NAs with nested for loop
+  
+  thePlaceToBe = environment()
+  apply(NApattern, MARGIN=1,
+           FUN=function(k){
+             i=k[1];j=k[2]
+             dat = get("data.to.impute", envir = thePlaceToBe)
+             dat[i,j] = NA
+             assign("data.to.impute",
+                    dat, envir = thePlaceToBe)
+           })
+  
+  #get mean and sds for scaling
+  mean.cols = as.vector(colMeans(data.to.impute, na.rm = T))
+  sd.cols = as.vector(apply(data.to.impute,2, function(col) sd(col, na.rm = T)))
+  # scale the data to impute
+  data.scaled = sweep(sweep(data.to.impute,
+                            MARGIN = 2,mean.cols,'-'),
+                      2,sd.cols,"/")
+  # put in right format for imputation
+  data.scaled = as.matrix(data.scaled)
+  
+  # -- if want scaled RMSE
+  # data.complete.scaled = sweep(sweep(data.complete,
+  #                                    MARGIN = 2,mean.cols,'-'),
+  #                              2,sd.cols,"/")
+  # # put in right format for imputation
+  # data.complete.scaled = as.matrix(data.complete.scaled)
+  
+  # BARE IN MIND THERE IS NO RANDOMNESS REGARDING impute.knn
+  # it has a default random seed inside it
+  # get list of imputed datasets for k = 1:20
+  predictions.k = lapply(c(1:50),
+                         function(x) 
+                           #knn.impute(data.scaled, k = x, cat.var = NULL)
+                         #knnImputation(data.scaled, k = x, scale = F)
+                         impute.knn(data.scaled, k = x,
+                                    rowmax = 1, colmax = 1)$data
+                         )
+  
+  # assert if there's any missing data in any of the imputed dataframes
+  stopifnot(sum(sapply(1:length(predictions.k),
+                       function(i) anyNA(predictions.k[[i]]))) == 0)
+  # rescale data
+  # -- if want scaled RMSE: predictions.k.rescaled = predictions.k 
+  predictions.k.rescaled = lapply(1:length(predictions.k),
+                                  function(x)
+                                    sweep(sweep(predictions.k[[x]],
+                                                MARGIN = 2,sd.cols,'*')
+                                          ,2,mean.cols,"+"))
+  # get amount of missing data
+  missing.dat = sum(is.na(data.scaled))
+  
+  ## MAYBE COMPUTE ERROR PER COLUMN AND GIVE BOXPLOT OF THAT
+  ## try logging bio because of skewed variables
+  # Calculate mean square error
+  # apply for every dataset
+  MSE = sapply(1:length(predictions.k),function(L){
+    # mean of vector of squared differences between vector and original data
+    mean(apply(NApattern, MARGIN=1,
+               FUN=function(k){
+                 i=k[1];j=k[2]
+                 # -- if want scaled RMSE
+                 # (predictions.k.rescaled[[L]][i,j]-data.complete.scaled[i,j])**2
+                 (predictions.k.rescaled[[L]][i,j]-data.complete[i,j])**2
+               }))
+  })
+
+  # calculate RMSE to have 
+  RMSE = sqrt(MSE)
+  RMSE
+  
+}
+
+RMSE = t(sapply(1:5,function(x) kNNImputeOptimization(log10(bio), seed = x)))
+boxplot(RMSE)
+best.k.med = which.min(apply(RMSE, 2, median))
+best.k.mean = which.min(colMeans(RMSE))
+
+# ?scaling: works weird man
+mean.cols = as.vector(colMeans(bio, na.rm = T))
+sd.cols = as.vector(apply(bio,2, function(col) sd(col, na.rm = T)))
+bio.scaled = sweep(sweep(bio,MARGIN = 2,mean.cols,'-'),2,sd.cols,"/")
+bio.scaled = as.matrix(bio.scaled)
+bio.scaled.imp = impute.knn(bio.scaled)$data
+
+#descale 0r rescale, however you wanna call it
+bio.imp = sweep(sweep(bio.scaled.imp,MARGIN = 2,sd.cols,'*'),2,mean.cols,"+")
+# this weird
+# matrix(c(1,2,3,5,3,4), nrow = 2, byrow = T)*c(2,3,2)
+# this works
+#sweep(matrix(c(1,2,3,5,3,4), nrow = 2, byrow = T), 2, 
+      # c(2,3,2), "*")
+print(Sys.time() - t0) # takes about 1 minute
+
+
+saveRDS(bio.imp, file = "data/preprocessed/bioImputed.rds")
+
+
+
+#################################################################
+##                     Proccessing of SNPs                     ##
+#################################################################
+
+# removing any snps from the info file that are not included in the data provided by barbz
+snps = colnames(snp.original)
+snp.info = snp_info.original[snp_info.original$markername %in% snps, ]
+
+saveRDS(snp.info, file = "data/preprocessed/snpInfo.rds")
+
+
+# recoding 00 as NA,  01 as 0, 02 as 1, and 03 as 2
+snp <- snp.original
+
+for (i in colnames(snp)){
+  snp[,i] = as.numeric(snp[,i], multiple=TRUE)
+}
+
+snp[snp==0] <- NA
+snp[snp==1] <- 0
+snp[snp==2] <- 1
+snp[snp==3] <- 2
+
+# SNP'S DATA A BIT HARDER TO IMPUTE BECAUSE VALUES ARE EITHER 0,1 OR 2, 
+# SOME ENTRIES (PARTICUARLY LINEARLY 77 HAS A LOT OF THE SAME VALUES, =2 FOR COLUMN 77)
+# WHICH MAKES IT EASY TO DO IMPUTATION:
+# -- mean(snp[,77]==2, na.rm = T) == 0.995998
+# -- sum(colMeans(snp==0, na.rm = T)>0.7) = 0
+# -- sum(colMeans(snp==1, na.rm = T)>0.95) = 0
+# -- sum(colMeans(snp==2, na.rm = T)>0.95) = 9 # having these columns in results in a standard deviation of
+# 0 which does not allow us to scale the SNP values, BUT MAYBE SNPs don't need to be rescaled
+# -- sum(colMeans(snp==2, na.rm = T)>0.7) = 60
+snp = snp[,!(colMeans(snp==2,na.rm= T)>0.9)]
+
+# NOT REALLY SURE HOW TO IMPUTE SNPs, ASK BARBS
+
+RMSE.snps = t(sapply(1:10,function(x) kNNImputeOptimization(data.in = snp, seed = x)))
+boxplot(RMSE.snps)
+
+saveRDS(snp, "data/preprocessed/snpProcessed.rds")
+
+###Can do imputation of snps
+#First check that no people have all missing values and no snps have all missing values 
+max(rowSums(is.na(snp)))
+#max number of missing values for one person is 15/177
+max(colSums(is.na(snp)))
+#max number of missing values for one snp is 226/2000
+
