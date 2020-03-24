0a8ff75d72c42b653fca761251a91fc29caf9a2c
Luis Cluster <lc5415@cx1-101-16-3.cx1.hpc.ic.ac.uk>
2020-03-24 17:14:16 +0000

    little analysis script for stability analysis of lasso as that was done in job array

--- Scripts/.ipynb_checkpoints/SNPquickImpute-checkpoint.sh
+++
@@ -1,8 +0,0 @@
-#PBS -l walltime=02:00:00
-#PBS -l select=1:ncpus=1:mem=8gb
-#PBS -N quickSNPcomputation
-
-module load anaconda3/personal
-cd /rdsgpfs/general/user/lc5415/home/BEES_TDS/Scripts
-
-Rscript SNPquickImpute.sh
