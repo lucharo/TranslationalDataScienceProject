95a17ad5c1990251735e4887d20aa3469725316d
Luis Cluster <lc5415@cx1-101-16-3.cx1.hpc.ic.ac.uk>
2020-03-24 19:41:16 +0000

    Lasso stability analysis plots ready

--- Scripts/StabAnalysis.R
+++ Scripts/StabAnalysis.R
@@ -2,13 +2,33 @@
 library(ggplot2)
 library(dplyr)
 
-lasso.prop = apply(lasso.stab, 1, FUN = function(x) {
+platform = Sys.info()['sysname']
+if (platform == 'Linux'){
+  save_data = data_folder = "../FULLDATA/preprocessed/"
+  save_plots = "../FULLResults/"
+} else {
+  setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+  save_data = data_folder = "../data/preprocessed/"
+  save_plots = "../results/"
+}
+
+bio.imp = readRDS(paste0(data_folder,"bioImputedKNN.rds"))[,-1]
+
+save_plots = paste0(save_plots,"PenalisedReg/ArrayJob/")
+
+lasso.stab = readRDS(paste0(save_plots, "lassoStab1.rds"))
+quietly(sapply(2:100, FUN =  function(x) {
+  assign("lasso.stab", rbind(lasso.stab, readRDS(paste0(save_plots, "lassoStab",x,".rds"))),
+         envir = .GlobalEnv)}))
+lasso.stab = data.frame(lasso.stab)
+colnames(lasso.stab) = colnames(bio.imp)
+
+lasso.prop = apply(lasso.stab, 2, FUN = function(x) {
   sum(x != 0)/length(x)
 }) 
-names(lasso.prop) = colnames(X)
 
 resStabAnalysis = data.frame(
-  Biomarker = colnames(X),
+  Biomarker = colnames(bio.imp),
   PropSelected = lasso.prop
 )
 
