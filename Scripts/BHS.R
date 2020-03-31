d7b59cd9ade836ffdc3b25aec52faba8de7f0f4f
Luis Chaves <lc5415@ic.ac.uk>
2020-03-31 14:05:20 +0100

    made some changes to BHS for package impletmenation

--- Scripts/BHS.R
+++ Scripts/BHS.R
@@ -21,12 +21,12 @@ bio = readRDS(paste0(data_folder,"bioProcessed.rds"))
 bio.imp = readRDS(paste0(data_folder,"bioImputedKNN.rds"))
 cov = readRDS(paste0(data_folder,"covProcessed.rds"))
 
-bio.dict = readxl::read_xlsx("../Biomarker_annotation.xlsx")
-
-# make pretty names
-bio.dict$`Biomarker name` = make.names(bio.dict$`Biomarker name`)
-bio.dict$`Biomarker name` = sub("\\.\\.",".",
-                                bio.dict$`Biomarker name`)
+# bio.dict = readxl::read_xlsx("../Biomarker_annotation.xlsx")
+#
+# # make pretty names
+# bio.dict$`Biomarker name` = make.names(bio.dict$`Biomarker name`)
+# bio.dict$`Biomarker name` = sub("\\.\\.",".",
+#                                 bio.dict$`Biomarker name`)
 
 # for simplicity we merge cov and bio here
 bio.imp = merge(bio.imp, cov[,c("ID","age_cl","gender")], by = "ID")
@@ -36,32 +36,32 @@ bio.imp = bio.imp[,-1]
 
 bio.imp$age_cl = as.factor(bio.imp$age_cl)
 bio.imp$gender = as.factor(bio.imp$gender)
-
+#
 BHSCalculator = function(reference, stratified = F, bySystems = T){
-  # reference can take values: 
+  # reference can take values:
   # -- "More_is_bad_Mantej
   # or
   # -- "More_is_bad_paper
-  
+
   MoreIsBad = paste0("MoreIsBad.",reference)
-  
+
   quantile_check = function(column, reference, dataset){
     # the "reference" column in this function is the one containing the info on whether or not
     # a given molecule is harmful in excess or in shortage, there are two references:
     # -- One from the biomarkers available in the paper
-    # -- Another one created from expert knowledge (by Mantej) including more biomarkers than 
+    # -- Another one created from expert knowledge (by Mantej) including more biomarkers than
     # in the original model
-    
+
     # if the value of this reference column for a given biomarker is 1, it means that biomarker
     # is harmful in excess, in which case we select the 3rd quartile (75%) which will be our benchmark
     # MORE than this kind of benchmark ==> +1 BHS
-    
+
     if (bio.dict[bio.dict$`Biomarker name` == column,
                  reference] == 1){
         output = quantile(dataset[,column])[4]
         quartile = "3rd"
     }
-    # in the case the value of the reference column for a given biomarker is 0, it means the biomarker 
+    # in the case the value of the reference column for a given biomarker is 0, it means the biomarker
     # is harmful in shortage, in which case we select the 1st quartile (25%) which will be our bencmark
     # LESS than this kind of benchmark ==> +1 BHS
     else if (bio.dict[bio.dict$`Biomarker name` == column,
@@ -70,7 +70,7 @@ BHSCalculator = function(reference, stratified = F, bySystems = T){
         quartile = "1st"
       }
     # Store info on biomarker with missing reference value, still will be removed later
-    # In the references provided by Mantej, we wrote -1 for those biomarkers we were unsure if they 
+    # In the references provided by Mantej, we wrote -1 for those biomarkers we were unsure if they
     # were beneficial or harmful (or irrelevant)
     #
     else{
@@ -79,8 +79,8 @@ BHSCalculator = function(reference, stratified = F, bySystems = T){
     }
     list(output, quartile)
   }
-  
-  
+
+
   ##################################################################
   ##        First make a 'dictionary' storing the relevant        ##
   ##         quantiles for each biomarker and each strata         ##
@@ -92,35 +92,35 @@ BHSCalculator = function(reference, stratified = F, bySystems = T){
                                     Quartile = character(0),
                                     AgeClass = character(0),
                                     Gender = character(0))
-    
+
     for (gender in unique(bio.imp$gender)){
       for (age.class in unique(bio.imp$age_cl)){
-        
-        # getting the entries from the bio dataset where the cov$age_cl values are 
+
+        # getting the entries from the bio dataset where the cov$age_cl values are
         # equal to age.class
         bio.per.class = bio.imp[bio.imp$age_cl==age.class & bio.imp$gender == gender,
                                 -c(ncol(bio.imp)-1, ncol(bio.imp))]
         # -ncol(bio.imp) everywhere to avoid age_cl column
-        
+
         result = data.frame(Biomarker = colnames(bio.per.class),
                             data.frame(matrix(
                               unlist(
                                 lapply(colnames(bio.per.class),
                                        function(x) quantile_check(x, reference = MoreIsBad, bio.per.class))),
                               nrow=length(colnames(bio.per.class)),
-                              byrow=T), stringsAsFactors = F), 
+                              byrow=T), stringsAsFactors = F),
                             stringsAsFactors = F)
-        
+
         colnames(result)[2:3] = c("Quantile.value", "Quartile")
         result = cbind(result, AgeClass = age.class, Gender = gender)
-        
+
         relevant_quantiles = rbind(relevant_quantiles, result)
       }
     }
 
     # take only complete cases
     relevant_quantiles = relevant_quantiles[complete.cases(relevant_quantiles),]
-    
+
   } else {
     relevant_quantiles = data.frame(Biomarker = colnames(bio.imp[,-c(ncol(bio.imp)-1, ncol(bio.imp))]),
                                     data.frame(matrix(
@@ -128,17 +128,17 @@ BHSCalculator = function(reference, stratified = F, bySystems = T){
                                         lapply(colnames(bio.imp[,-c(ncol(bio.imp)-1, ncol(bio.imp))]),
                                                function(x) quantile_check(x, reference = MoreIsBad, bio.imp))),
                                       nrow=length(colnames(bio.imp[,-c(ncol(bio.imp)-1, ncol(bio.imp))])),
-                                      byrow=T), stringsAsFactors = F), 
+                                      byrow=T), stringsAsFactors = F),
                                     stringsAsFactors = F)
-    
-    
+
+
     colnames(relevant_quantiles)[2:3] = c("Quantile.value", "Quartile")
-    
+
     # take only complete cases
     relevant_quantiles = relevant_quantiles[complete.cases(relevant_quantiles),]
   }
 
-  
+
   #######################################################################
   ##  Second go through every biomarker (and each strata if relevant)  ##
   ##                and get the score for each biomarker               ##
@@ -150,23 +150,23 @@ BHSCalculator = function(reference, stratified = F, bySystems = T){
     # initialising empty dataframe with dims of however many biomarkers are being considered
     # could add two extra columns for each strata
     bio.score = bio.imp[F,unique(relevant_quantiles$Biomarker)]
-    
+
     for (gender in unique(bio.imp$gender)){
       for (age in unique(bio.imp$age_cl)){
         # select entries of biomarker for which age is equal to iterations
         bio.sub  = bio.imp[bio.imp$age_cl==age & bio.imp$gender == gender,
                            -c(ncol(bio.imp)-1, ncol(bio.imp))]
         # select quantiles for which age is equal to age iterations
-        relevant_quantiles.age.gender = relevant_quantiles[relevant_quantiles$AgeClass==age & 
+        relevant_quantiles.age.gender = relevant_quantiles[relevant_quantiles$AgeClass==age &
                                                              relevant_quantiles$Gender == gender,]
         attach(relevant_quantiles.age.gender)
         res = lapply(Biomarker, # for each biomarker from relevant_quantiles.age.gender
                      function(x){ # if the biomarker is bad in excess (i.e. boundary is 3rd quartile)
                        # return the element-wise comparison to the relevant quartile value
                        ifelse(Quartile[Biomarker == x] == "3rd",
-                              return(bio.sub[,x] > 
+                              return(bio.sub[,x] >
                                        Quantile.value[Biomarker == x]),
-                              return(bio.sub[,x] < 
+                              return(bio.sub[,x] <
                                        Quantile.value[Biomarker == x]))
                      })
         res = data.frame(matrix(unlist(res),
@@ -176,18 +176,18 @@ BHSCalculator = function(reference, stratified = F, bySystems = T){
         colnames(res) = Biomarker
         #res$AgeCl = age
         bio.score = rbind(bio.score, res)
-        
+
         # stopifnot(all(bio.imp$age_cl[bio.imp$age_cl==age]==res$AgeCl))
         # stopifnot(all(rownames(bio.imp$age_cl[bio.imp$age_cl==age])==rownames(res$AgeCl)))
-        
+
         detach(relevant_quantiles.age.gender)
       }
     }
 
-  
+
     # I actually don't need the age class column just need them to keep the original index
     #bio.score = bio.score[,-c(ncol(bio.score)-1, ncol(bio.score))]
- 
+
   }else{
     attach(relevant_quantiles)
     # bio.score is a list containing for each biomarker (first subset of list)
@@ -199,9 +199,9 @@ BHSCalculator = function(reference, stratified = F, bySystems = T){
                                 return(bio.imp[,x] > Quantile.value[Biomarker == x]),
                                 return(bio.imp[,x] < Quantile.value[Biomarker == x]))
                        })
-    
+
     # bio.score is then turned into a nice data frame where each biomarker is a column
-    # and each row is an individual 
+    # and each row is an individual
     # note: unlist returns a long vector which results from unlist the bio.score list into
     # a vector with a length equal to the number of elements in the original list.
     # byrow in this case must be equal to FALSE
@@ -219,8 +219,8 @@ BHSCalculator = function(reference, stratified = F, bySystems = T){
   ##    3rd and finally get the mean score for each individual    ##
   ##              and sort in same order as bio.imp               ##
   ##################################################################
-  
-  
+
+
   if (bySystems){
     systems.ref = paste0("System.",reference)
     bio.score$ID = rownames(bio.score)
@@ -231,7 +231,7 @@ BHSCalculator = function(reference, stratified = F, bySystems = T){
     bio.score = bio.score %>% dplyr::group_by(ID, System) %>% dplyr::summarise(BHSsystem = mean(Amount))
     # the line below would give you dataframe with score by system
     # bio.score %>% spread(System, BHS)
-    
+
     bio.score = bio.score %>% dplyr::group_by(ID) %>% dplyr::summarise(total_score = mean(BHSsystem))
     bio.score = as.data.frame(bio.score) # Setting row names on a tibble is deprecated.
     rownames(bio.score) = bio.score$ID
@@ -276,7 +276,7 @@ fig = data.frame(rbind(
   cbind(BHS = scores_Mantej, Reference = "Mantej"),
   cbind(BHS = scores_Barbara, Reference = "Barbara"),
   cbind(BHS = cov$BS2_all[cov$ID %in% ScoresBarbara$ID], Reference = "BS2")),
-      stringsAsFactors = F) %>% 
+      stringsAsFactors = F) %>%
   ggplot(aes(x = Reference, y = as.numeric(BHS), fill = Reference))+
   geom_boxplot()+
   ylab("BHS")+
