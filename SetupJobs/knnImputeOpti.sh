05ea1efd1abb0c9577306abc32f8fda2bf03b06d
Luis Cluster <lc5415@cx1-101-16-3.cx1.hpc.ic.ac.uk>
2020-04-09 13:26:31 +0100

    cluster update

--- SetupJobs/knnImputeOpti.sh
+++ SetupJobs/knnImputeOpti.sh
@@ -1,9 +1,11 @@
 #PBS -l walltime=48:00:00
-#PBS -l select=1:ncpus=40:mem=480gb
+#PBS -l select=1:ncpus=8:mem=96gb
 #PBS -N knnImputeOpti
-
+#PBS -J 1-20
 module load anaconda3/personal
 
 cd /rdsgpfs/general/user/lc5415/home/BEES_TDS/Scripts
 
-Rscript knnImputation.R
+seed=$PBS_ARRAY_INDEX
+
+time Rscript knnOptimizationSingle.R $seed
