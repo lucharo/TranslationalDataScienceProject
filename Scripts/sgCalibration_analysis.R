55c4830f5c17da623c60cff10a831b6eb2de95f8
Luis Cluster <lc5415@cx1-101-16-3.cx1.hpc.ic.ac.uk>
2020-04-09 13:37:02 +0100

    I hatae the cluster

---
+++ Scripts/sgCalibration_analysis.R
@@ -0,0 +1,27 @@
+
+library(ggplot2)
+library(dplyr)
+library(ggpubr)
+
+platform = Sys.info()['sysname']
+if (platform == 'Linux'){
+  save_data = data_folder = "../FULLDATA/preprocessed/"
+  save_plots = "../FULLResults/PLS/"
+} else {
+  setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+  save_data = data_folder = "../data/preprocessed/"
+  save_plots = "../results/"
+}
+
+
+read_data = paste0(save_plots,"ArrayJob/")
+
+sg_calib = readRDS(paste0(read_data, "sgCalibration_1.rds"))
+sapply(3:56, FUN =  function(x) {
+  assign("sg_calib", 
+         rbind(sg_calib, readRDS(paste0(read_data, "sgCalibration_", x,".rds"))),
+         envir = .GlobalEnv)})
+
+sg_calib = as.data.frame(sg_calib)
+
+
