2c3a0c28f30f139a53351b3196178d00621f67df
Luis Chaves <lc5415@ic.ac.uk>
2020-04-20 20:14:05 +0100

    removed .ipynb_checkpoints

--- SetupJobs/.ipynb_checkpoints/BHS-checkpoint.sh
+++
@@ -1,9 +0,0 @@
-#PBS -l walltime=1:00:00
-#PBS -l select=1:ncpus=1:mem=8gb
-#PBS -N BHSStratAgeGender
-module load anaconda3/personal
-source activate TDS
-cd /rdsgpfs/general/user/lc5415/home/BEES_TDS/Scripts
-
-time Rscript BHS.R
-conda deactivate
